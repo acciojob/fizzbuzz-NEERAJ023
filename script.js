1aaa3e88635ca1f6e1221826a00b4619de37289a
NEERAJ023 <143694023+NEERAJ023@users.noreply.github.com>
2023-11-15 20:27:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+function neeraj(){
+	for(let i=1; i<=20; i++){
+		if(i%3==0){
+		if(i%5==0){
+				alert("FizzBuzz");
+			}else{
+			alert("Fizz");
+			}
+			
+		}else if(i%5==0){
+			alert("Buzz")
+		}else{
+			alert(i);
+		}
+	}
+}
+neeraj();
